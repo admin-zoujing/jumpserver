a663c03b6a6b3912b09f487d152b089d1cd8f7b5
root <root@localhost.localdomain>
2019-05-27 10:47:27 +0800

    first commit

--- jumpserver/apps/assets/models/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-#
-from .user import AdminUser, SystemUser
-from .label import Label
-from .cluster import *
-from .group import *
-from .asset import *
-from .utils import *
--- jumpserver/apps/assets/views/__init__.py
+++
@@ -1,8 +0,0 @@
-# coding:utf-8
-from .asset import *
-from .group import *
-from .cluster import *
-from .system_user import *
-from .admin_user import *
-from .label import *
-
